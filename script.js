53c79acae690697f5f2d4bcc9b49e5de8cb90bbe
DouglasMacKrell <douglasmackrell@pursuit.org>
2020-11-27 23:56:50 -0500

    text to speech complete

--- script.js
+++ script.js
@@ -4,32 +4,43 @@ const stopButton = document.getElementById("stop-button");
 const textInput = document.getElementById("text");
 const speedInput = document.getElementById("speed");
 
-playButton.addEventListener('click', () => {
-    playText(textInput.value)
+playButton.addEventListener("click", () => {
+  playText(textInput.value);
+});
+
+pauseButton.addEventListener("click", pauseText);
+
+stopButton.addEventListener("click", stopText);
+
+speedInput,addEventListener("input", () => {
+    stopText()
+    playText(utterance.text.substring(currCharacter))
 })
 
-pauseButton.addEventListener('click', pauseText)
-
-stopButton.addEventListener('click', stopText)
+const utterance = new SpeechSynthesisUtterance();
+utterance.addEventListener("end", () => {
+  textInput.disabled = false;
+});
+utterance.addEventListener("boundary", (event) => {
+  currCharacter = event.charIndex;
+});
 
 function playText(text) {
-    if (speechSynthesis.pause && speechSynthesis.speaking) {
-        return speechSynthesis.resume()
-    }
-    const utterance = new SpeechSynthesisUtterance(text)
-    utterance.rate = speedInput.value || 1
-    utterance.addEventListener('end', () => {
-        textInput.disabled = false
-    })
-    textInput.disabled = true
-    speechSynthesis.speak(utterance)
+  if (speechSynthesis.pause && speechSynthesis.speaking) {
+    return speechSynthesis.resume();
+  }
+  if (speechSynthesis.speaking) return
+  utterance.text = text
+  utterance.rate = speedInput.value || 1;
+  textInput.disabled = true;
+  speechSynthesis.speak(utterance);
 }
 
 function pauseText() {
-    if (speechSynthesis.speaking) speechSynthesis.pause()
+  if (speechSynthesis.speaking) speechSynthesis.pause();
 }
 
 function stopText() {
-    speechSynthesis.resume()
-    speechSynthesis.cancel()
+  speechSynthesis.resume();
+  speechSynthesis.cancel();
 }
