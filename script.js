fa6dafb1fad8160ffc818a26c933d8c33dd89f95
DouglasMacKrell <douglasmackrell@pursuit.org>
2020-11-27 23:33:45 -0500

    play and pause active

--- script.js
+++ script.js
@@ -8,8 +8,21 @@ playButton.addEventListener('click', () => {
     playText(textInput.value)
 })
 
+pauseButton.addEventListener('click', pauseText)
+
 function playText(text) {
+    if (speechSynthesis.pause && speechSynthesis.speaking) {
+        return speechSynthesis.resume()
+    }
     const utterance = new SpeechSynthesisUtterance(text)
     utterance.rate = speedInput.value || 1
+    utterance.addEventListener('end', () => {
+        textInput.disabled = false
+    })
+    textInput.disabled = true
     speechSynthesis.speak(utterance)
+}
+
+function pauseText() {
+    if (speechSynthesis.speaking) speechSynthesis.pause()
 }
